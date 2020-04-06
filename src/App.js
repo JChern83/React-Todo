185ce3a92922264cf7e2886ae01f66cf97513db4
Juan C Hernandez <JChern83@users.noreply.github.com>
2020-04-06 14:34:50 -0700

    working properly for the most part, working on styling next

--- src/App.js
+++ src/App.js
@@ -1,16 +1,83 @@
 import React from 'react';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+import './Todo.css'
+
+
+const tasks = [
+  {name: "", id: 1, completed: false}
+]
+
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Lets Get started!</h2>
-      </div>
-    );
+
+  constructor() {
+    super();
+    this.state = {
+      tasks
+    }
   }
+  toggleItem = (event, itemId) => {
+    event.preventDefault();
+
+    this.setState({
+      tasks: this.state.tasks.map(item => {
+        if (item.id === itemId) {
+          return {
+            ...item, 
+            completed: !item.completed
+          } 
+        } else {
+          return item
+        }
+      })
+    })
+  }
+
+  completedTask = event => {
+    event.preventDefault();
+
+    this.setState({
+      tasks: this.state.tasks.filter(item => {
+        return !item.completed
+      })
+    })
+  }
+
+  addTask = (event, taskName) => {
+    const newTask = {
+      id: Date.now(),
+      name: taskName,
+      completed: false
+    }
+
+    this.setState({
+      tasks: [newTask, ...this.state.tasks]
+    })
+  }
+
+
+render() {
+  return (
+    <div className="container">
+    <h2>Welcome to your Todo App!</h2>
+    <div className="todo">
+      <TodoForm 
+        addTodo={this.addTask}
+        tasks={this.state.tasks}
+        />
+    </div>
+    <div className="todo-container">
+        <TodoList
+          tasks= {this.state.tasks}
+          toggleItem= {this.toggleItem} 
+          completedTask= {this.completedTask} 
+        />
+    </div>
+    </div>
+);
+}
 }
 
 export default App;
